96024638515aab777be93cb20760203a3371c647
Zane Gilmore <zane.gilmore@gmail.com>
2023-01-06 15:32:59 +1300

    going with 7seg display

--- python/scales_main.py
+++ python/scales_main.py
@@ -1,7 +1,7 @@
 from machine import Pin,SPI
 from hx711 import HX711
 import max7219_8digit
-
+import time
 
 OFFSET = 4650
 GRAM_COEFF = 0.002912589
@@ -16,9 +16,11 @@ ss = Pin(5, Pin.OUT)
 # init the display
 display = max7219_8digit.Display(spi, ss)
 
-leftstr = "gr -"
+leftstr = "gr  "
 display_str = "{}8  8".format(leftstr)
 display.write_to_buffer(display_str)
+
+
 
 def write_right(the_str):
     if len(the_str) > 4:
@@ -59,16 +61,25 @@ class scales(object):
         self.find_offset()
 
     def read_loop(self):
+        global leftstr
         while(not self.checking_tare):
             
             raw_reading = self.adc.read()
             
-            print("raw={}".format(raw_reading))
+            #print("raw={}".format(raw_reading))
             
             grams = (raw_reading - self.offset) * self.coeff
+            if grams > 1000:
+                leftstr = "kg  "
+                write_right("{:.3f}".format(grams/1000))
+            else:
+                leftstr = "gr  "
+                write_right("{:.0f}".format(grams))
             
-            print("grams={}".format(grams))
-            write_right("{:.0f}".format(grams))
+            #print("grams={}".format(grams))
+                
+            time.sleep(0.1)
+
             
         
 
@@ -91,7 +102,7 @@ class scales(object):
         self.checking_tare = True
         
         avg = self.avg_reading()
-        print("offset = {}".format(avg))
+        print("zzoffset = {}".format(avg))
         
         self.offset = avg
         self.checking_tare = False
@@ -104,41 +115,43 @@ class scales(object):
         print("coeff = {}".format(self.coeff))
         
     
-##################################################################
-### Instantiate the scales
-the_scales = scales(30)
-the_scales.read_loop()
-
-
-
-
 ###########################################################
 ###             Tare Button                          #######
 ###########################################################
 # set up tare button
-tare_button = Pin(21, Pin.IN, Pin.PULL_UP)
+tare_button = Pin(16, Pin.IN, Pin.PULL_UP)
 
 def tare_button_callback(p):
+    global the_scales
+    global tare_button
     # the tare button has been pressed
     # first handle the debounce
+
+    print("!!!!!!!!! tare button pressed !!!!!!!!")
+    write_right("8888")
     tare_button.irq(handler=None)
-    
-    ########## Do what needs to be done
-    the_scales.find_offset()
-    
-    
-    # now wait for the release of the button
-    # wait for button to settle
-    tare_button_count = 0
-    while tare_button_count < 2:
-        time.sleep(0.1)
-        # check if button released
-        if tare_button.value() == 1:
-            tare_button_count+=1
-            
-    
+    # there appears to be a pretty serious debounce problem
+    if tare_button.value() == 0:
+        # now wait for the release of the button
+        # wait for button to settle
+        tare_button_count = 0
+        print("tare_button_value={}".format(tare_button.value()))
+        while tare_button_count < 3:
+            # check if button released
+            print("tare_button_value={}".format(tare_button.value()))
+            if tare_button.value() == 1:
+                tare_button_count+=1
+            elif tare_button.value() == 0:
+                tare_button_count = 0
+            time.sleep(0.05)
+
+        ########## Do what needs to be done
+        the_scales.find_offset()
+
+
+    print("!!!!!!!!! tare button released !!!!!!!!")
     tare_button.irq(trigger=Pin.IRQ_FALLING, handler=tare_button_callback)
-    the_scales.read_loop()
+    
 
 
 tare_button.irq(trigger=Pin.IRQ_FALLING, handler=tare_button_callback)
@@ -146,4 +159,7 @@ tare_button.irq(trigger=Pin.IRQ_FALLING, handler=tare_button_callback)
 
     
     
-    
+##################################################################
+### Instantiate the scales
+the_scales = scales(30)
+the_scales.read_loop()
